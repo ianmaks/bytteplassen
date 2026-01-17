a72d824cbaee4880c5cb58f69b72e3dd3f0874d1
Halvor Øverby <halvor.overby@gmail.com>
2026-01-17 17:00:01 +0100

    mix migrations

--- market/settings.py
+++ market/settings.py
@@ -30,8 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "XA?[edoQ()gNgk^44wpRo7\YZ}@L.~b4JX%_|~y&qEBgULy+_n"
-#os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
