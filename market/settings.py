2b6d72575c76e80e203284db5507d93a90b5be2f
Ian McGregor Aksum <ian.aksum@gmail.com>
2026-01-17 10:58:38 +0100

    Changed env loading

--- market/settings.py
+++ market/settings.py
@@ -26,8 +26,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "XA?[edoQ()gNgk^44wpRo7\YZ}@L.~b4JX%_|~y&qEBgULy+_n"
