a7e8e331e29407e48207eaf759a614013e20371c
Halvor Øverby <halvor.overby@gmail.com>
2026-01-17 09:42:59 +0100

    fix static file issue

--- market/settings.py
+++ market/settings.py
@@ -123,11 +123,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# This setting defines the single folder you want to collect all your static files into.
+# This is required when using the staticfiles app, especially for production and collectstatic.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
