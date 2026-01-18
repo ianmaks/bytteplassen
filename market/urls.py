32e546831e594d77cfa24382367b36b2c757b5c9
Halvor Øverby <halvor.overby@gmail.com>
2026-01-18 08:30:07 +0100

    register new user

--- market/urls.py
+++ market/urls.py
@@ -18,11 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from users.views import register
 
 urlpatterns = [
     path("", auth_views.LoginView.as_view(), name="login"),    
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
     path("marketplace/", include("marketplace.urls")),
+    path("register/", register, name="register"),
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from users.forms import RegistrationForm
 
 # Create your views here.
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    return render(request, 'registration/register.html', {'form': form})
